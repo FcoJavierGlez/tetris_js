f2fe27e5ee8ba0e7307b5738385995d3f1df22bc
FcoJavierGlez <FJGS_86@hotmail.com>
2020-12-25 09:25:06 +0100

    Updated main.js file. UI render can being paused now.

--- js/main.js
+++ js/main.js
@@ -45,15 +45,34 @@
         const tetris    = new TetrisGame(20,10,localStorage.getItem('tetris_max_score') == null ? 0 : localStorage.getItem('tetris_max_score'));
         let [boardGame,fragment]             = createGrid(20,10,true);
         let [nextPieceDOM,fragmentPreviewer] = createGrid(2,4);
+        let idPause = 0;
+
+        const playRender = (boardGameDOM,nextPieceDOM,level,lines,score,game) => {
+            return setInterval(
+                () => {
+                    renderGrid(boardGameDOM,game.getBoardGame());
+                    renderGrid(nextPieceDOM,game.getInfoPreviewNextPiece());
+                    level.innerHTML = game.getLevel();
+                    lines.innerHTML = game.getLines();
+                    score.innerHTML = game.getScore();
+                }, 50);
+        }
+    
+        const pauseRender = () => {
+            clearInterval(idPause);
+            idPause = 0;
+        }
+    
+        const togglePause = (boardGameDOM,nextPieceDOM,level,lines,score,game) => idPause == 0 ? (idPause = playRender(boardGameDOM,nextPieceDOM,level,lines,score,game)) : pauseRender(idPause);
 
         board.appendChild(fragment);
         previewer.appendChild(fragmentPreviewer);
-
-        render(boardGame,nextPieceDOM,level,lines,score,tetris);
-
+        
         document.addEventListener("keydown", e => {
-            if (e.code == 'Space')
+            if (e.code == 'Space') {
                 tetris.togglePause();
+                togglePause(boardGame,nextPieceDOM,level,lines,score,tetris);
+            }
             else if (e.code == 'ArrowLeft' || e.code == 'ArrowRight')
                 tetris.movePiece( e.code == 'ArrowLeft' ? 'LEFT' : 'RIGHT');
             else if (e.code == 'ArrowUp')
