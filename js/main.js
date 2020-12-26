15925fd6b8bd4707e9f329d91d92ec64f58b2579
FcoJavierGlez <FJGS_86@hotmail.com>
2020-12-26 15:37:22 +0100

    Updated main.js

--- js/main.js
+++ js/main.js
@@ -25,13 +25,24 @@
                 elementsDOM[i][j].classList = array[i][j] == ' ' ? 'square' : `${array[i][j]}`;
     }
 
+    const setMaxScoreIntoLocalStorage = maxScore => {
+        if (!(maxScore > localStorage.getItem('tetris_max_score'))) return;
+        localStorage.removeItem('tetris_max_score');
+        localStorage.setItem('tetris_max_score',maxScore);
+    }
+
     const init = () => {
-        const board     = document.getElementById("board");
-        const previewer = document.getElementById("next-piece");
-        const level     = document.getElementById("level");
-        const lines     = document.getElementById("lines");
-        const score     = document.getElementById("score");
-        const tetris    = new TetrisGame(localStorage.getItem('tetris_max_score') == null ? 0 : localStorage.getItem('tetris_max_score'),20,10);
+        const board       = document.getElementById("board");
+        const previewer   = document.getElementById("next-piece");
+        const level       = document.getElementById("level");
+        const lines       = document.getElementById("lines");
+        const score       = document.getElementById("score");
+        const pauseScreen = document.getElementById("pause-screen");
+        const divPause    = document.getElementById("pause");
+        const divGameOver = document.getElementById("game-over");
+        const divMessage  = document.getElementById("content-message");
+        const resetButton = document.getElementById("reset");
+        const tetris      = new TetrisGame(localStorage.getItem('tetris_max_score') == null ? 0 : localStorage.getItem('tetris_max_score'),20,10);
         let [boardGame,fragment]             = createGrid(20,10,true);
         let [nextPieceDOM,fragmentPreviewer] = createGrid(2,4);
         let idPause = 0;
@@ -40,10 +51,30 @@
         lines.innerHTML = tetris.getLines();
         score.innerHTML = tetris.getScore();
 
+        const showPauseScreen = endGame => {
+            pauseScreen.className = idPause == 0 || endGame ? 'pauseScreen' : 'hidden';
+            endGame ? (
+                divPause.className = 'hidden',
+                divGameOver.className = 'game-over',
+                divMessage.innerHTML = `
+                    <p>Has obtenido ${tetris.getScore()} puntos.</p>
+                    <p>Tu máxima puntuación obtenida ha sido de ${localStorage.getItem('tetris_max_score')} puntos.</p>
+                    <p>¿Deseas volver a intentarlo?</p>`
+            ) : (
+                divPause.className = 'pause',
+                divGameOver.className = 'hidden'
+            );
+        }
+
         const playRender = (boardGameDOM,nextPieceDOM,level,lines,score,game) => {
             return setInterval(
                 () => {
-                    game.getEndGame() ? pauseRender() : false;
+                    if (game.getEndGame()) {
+                        setMaxScoreIntoLocalStorage(TetrisGame.getMaxScore());
+                        showPauseScreen(true);
+                        pauseRender();
+                        return;
+                    }
                     renderGrid(boardGameDOM,game.getBoardGame());
                     renderGrid(nextPieceDOM,game.getInfoPreviewNextPiece());
                     level.innerHTML = game.getLevel();
@@ -67,6 +98,7 @@
             if (e.code == 'Space') {
                 tetris.togglePause();
                 togglePause(boardGame,nextPieceDOM,level,lines,score,tetris);
+                showPauseScreen();
             }
             else if (e.code == 'ArrowLeft' || e.code == 'ArrowRight')
                 tetris.movePiece( e.code == 'ArrowLeft' ? 'LEFT' : 'RIGHT');
@@ -74,7 +106,19 @@
                 tetris.rotatePiece();
             else if (e.code == 'ArrowDown')
                 tetris.descendPiece();
-        })
+        });
+
+        resetButton.addEventListener("click", () => {
+            tetris.reset();
+            renderGrid(boardGame,tetris.getBoardGame());
+            for (let i = 0; i < nextPieceDOM.length; i++) // reset render next piece
+                for (let j = 0; j < nextPieceDOM[0].length; j++) 
+                    nextPieceDOM[i][j].classList = 'square';
+            level.innerHTML = tetris.getLevel();
+            lines.innerHTML = tetris.getLines();
+            score.innerHTML = tetris.getScore();
+            showPauseScreen();
+        });
 
     }
     document.addEventListener("DOMContentLoaded",init);
